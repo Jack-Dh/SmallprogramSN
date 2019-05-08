e27ab015ecf7003065f28e6e0d77f8f4cd6c242d
Zmyhh <897268593@QQ.com>
2019-05-08 20:52:57 +0800

    我的派工单下拉刷新，上滑加载更多

--- stroe/index.js
+++ stroe/index.js
@@ -6,6 +6,7 @@ const store = new Vuex.Store({
   nickname : "未设置",
   loginApi:'http://192.168.2.241:8099/supplychain/api/operator/login',//登录
   dispatchlist:'http://192.168.2.241:8099/supplychain/api/dispatch/list',//我的派单信息分页查询
+  dispatchDetailsbyID:'http://192.168.2.241:8099/supplychain/api/dispatch/select'//根据UUID查询派工单详情
  
  }
 });
